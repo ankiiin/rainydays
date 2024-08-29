026f916d0a614caa4e380baaf76be3710c8a5e31
ankiiin <annham49344@stud.noroff.no>
2024-08-29 19:22:32 +0200

    Productlist

---
+++ JS/productsList.js
@@ -0,0 +1,11 @@
+export const products = [
+    {
+      Image: "sailor_jacket.png",
+      Title: "Sailor Jacket",
+      Description: "The Women's Sailor jacket is bound to be your new go-to water-repellent rain jacket.",
+      Gender: "Female",
+      Sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+      Basecolor: "Orange, grey",
+      Price: "$ 49.99"
+    }
+]
